d2b2ee4ccb299f0628234f42d2ac78a8b04b7cbe
PauBlanes_ENTI <paublanespardo@enti.cat>
2018-03-08 13:36:02 +0100

    cambiat socket selector per vector de sockets que es mes comode
    A la practica ja es veura pro de moment per el chat millor aixi

--- Taller4/Peer/Main.cpp
+++ Taller4/Peer/Main.cpp
@@ -26,7 +26,7 @@ int main()
 	unsigned short myPort = sock.getLocalPort();	
 	sock.disconnect();
 	
-	SocketSelector selector;
+	vector < TcpSocket*> sockets;
 	int currentPeers; string newIp; unsigned int newPort;
 	packet >> currentPeers;
 	
@@ -37,11 +37,11 @@ int main()
 		TcpSocket *newPeer = new TcpSocket;
 		Socket::Status s = newPeer->connect(newIp, newPort); //teoricament el newPort esta b pero si el poso aqui no va
 		if (s == Socket::Done) {
-			selector.add(*newPeer);
+			sockets.push_back(newPeer);
 			cout << "conectado al peer con ip : " << newIp << " en su puerto : " << newPort << endl;
 		}
 		else {
-			cout << "Error de tipo " << s <<" al conectar con peer con ip : " << newIp << " en el puerto " << newPort << endl;
+			cout << "Error al conectar con peer con ip : " << newIp << " en su puerto " << newPort << endl;
 		}
 
 		delete newPeer;					
@@ -54,15 +54,14 @@ int main()
 			cout << "port error" << endl;
 		else
 			cout << "port " << myPort << " ok" << endl;
-		selector.add(listener);
-
+		
 		for (int i = 0; i < OTHER_PLAYERS - currentPeers; i++) {
 			cout << "ESPERANDO NUEVOS JUGADORES..." << endl;
 			TcpSocket *newPeer = new TcpSocket;
 			Socket::Status s = listener.accept(*newPeer);
 			if (s == Socket::Done) {
-				selector.add(*newPeer);
-				cout << "conectado al peer con ip : " << newPeer->getRemoteAddress() << "en su puerto : " << newPeer->getRemotePort() << endl;
+				sockets.push_back(newPeer);
+				cout << "conectado al peer con ip : " << newPeer->getRemoteAddress() << endl;
 			}
 			else
 				cout << "error al aceptar nuevo peer" << endl;
@@ -73,5 +72,8 @@ int main()
 			
 	cout << "TODOS LOS PEERS SE HAN CONECTADO CORRECTAMENTE!" << endl;
 	system("pause");
+
+	//AQUI JA EL CHAT PROPIAMENT
+
 	return 0;
 }
